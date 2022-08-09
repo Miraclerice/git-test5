b8c23ecfe5e43f3d5fdc3c465385c2c1f5942380
BingxianXie <1669176771@qq.com>
2022-08-10 04:00:16 +0800

    second commit

--- src/main/java/com/itxie/git/GitTest.java
+++ src/main/java/com/itxie/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itxie.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gity!");
+        System.out.println("hello gity!");
     }
 }
