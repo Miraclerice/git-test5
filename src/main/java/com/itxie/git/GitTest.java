256f7c27a2111e27ac35401f49fe4cf3658375a3
BingxianXie <1669176771@qq.com>
2022-08-10 04:01:15 +0800

    third commit

--- src/main/java/com/itxie/git/GitTest.java
+++ src/main/java/com/itxie/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gity!");
         System.out.println("hello gity!");
+        System.out.println("hello git3!");
     }
 }
